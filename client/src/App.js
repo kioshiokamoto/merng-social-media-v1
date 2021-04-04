7d33ea0be8e92ec7136fc0736a38f65313d31246
Kioshi Okamoto <kjor29@gmail.com>
2021-04-03 19:40:52 -0500

    :dart: IMPROVE: instalacion de modulos: graphql, moment, semantic-ui, creacion de pages y componentes: MenuBar y PostCard

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css'
+
+import {Container} from 'semantic-ui-react'
+
+//Pages
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import MenuBar from './components/MenuBar';
 
 function App() {
 	return (
-		<div>
-			<h1>Start!</h1>
-		</div>
+		<Router>
+			<Container>
+			<MenuBar />
+			<Route exact path="/" component={Home}/>
+			<Route exact path="/login" component={Login} />
+			<Route exact path="/register" component={Register} />
+			</Container>
+		</Router>
 	);
 }
 
---
+++ client/src/pages/Home.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Grid, Image } from 'semantic-ui-react';
+import PostCard from '../components/PostCard';
+
+const Home = () => {
+	const {
+		loading,
+		data: { getPosts: posts } = {},
+	} = useQuery(FETCH_POST_QUERY);
+
+	if (posts) {
+		console.log(posts);
+	}
+	return (
+		<Grid columns={3}>
+			<Grid.Row className="page-title">
+				<h1>Recent Posts</h1>
+			</Grid.Row>
+			<Grid.Row>
+				{loading ? (
+					<h1>Loading posts...</h1>
+				) : (
+					posts &&
+					posts.map((post) => (
+						<Grid.Column key={post.id} style={{marginBottom:20}}>
+							<PostCard post={post} />
+						</Grid.Column>
+					))
+				)}
+			</Grid.Row>
+		</Grid>
+	);
+};
+
+const FETCH_POST_QUERY = gql`
+	{
+		getPosts {
+			id
+			body
+			createdAt
+			username
+			likeCount
+			likes {
+				username
+			}
+			commentCount
+			comments {
+				id
+				username
+				createdAt
+				body
+			}
+		}
+	}
+`;
+
+export default Home;
